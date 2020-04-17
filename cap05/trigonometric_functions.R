ad87fe5e54602fb3e8338a61919c8296c5439493
vcwild <vcwildm@gmail.com>
2020-04-17 00:32:33 -0300

    added trigonometric functions

---
+++ cap05/trigonometric_functions.R
@@ -0,0 +1,19 @@
+# Trigonometric Functions
+# Description
+# These functions give the obvious trigonometric functions. They respectively compute the cosine, sine, tangent, arc-cosine, arc-sine, arc-tangent, and the two-argument arc-tangent.
+# 
+# cospi(x), sinpi(x), and tanpi(x), compute cos(pi*x), sin(pi*x), and tan(pi*x).
+# 
+# Usage
+# cos(x)
+# sin(x)
+# tan(x)
+# 
+# acos(x)
+# asin(x)
+# atan(x)
+# atan2(y, x)
+# 
+# cospi(x)
+# sinpi(x)
+# tanpi(x)
