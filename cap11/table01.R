4a8d536638a52d3775ea27698224dc185a506514
vcwild <vcwildm@gmail.com>
2020-04-22 09:00:17 -0300

    add tables

---
+++ cap11/table01.R
@@ -0,0 +1,3 @@
+# Define NA Values
+
+read_csv("a, b, c \n1, 2, .", na = ".")
---
+++ cap11/table02.R
@@ -0,0 +1,9 @@
+read_csv("a,b,c\n1,2,3\n4,5,6")
+
+read_csv("a,b,c,d\n1,2\n1,2,3,4")
+
+read_csv("a,b\n1, NA")
+
+read_csv("a,b\n1,2\na,b")
+
+read_csv("a,b\n1,3")
